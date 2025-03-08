341f48005e1753e9262dbc5fc512d6a331dd5a3f
Nigel Coutinho <66671282+sagginigel@users.noreply.github.com>
2025-03-08 22:52:50 +0400

    Create delete_dot_txt_filles.bat
    Delete all files from directories and subdirectories .txt extension

---
+++ delete_dot_txt_filles.bat
@@ -0,0 +1,9 @@
+@echo off
+del /s /q *.txt
+echo All .txt files have been deleted.
+
+rem @echo off: Disables the echoing of commands in the console.
+rem del: Deletes files.
+rem /s: Deletes files in the current directory and all subdirectories.
+rem /q: Quiet mode, which suppresses confirmation prompts.
+rem *.txt: Specifies all files with the .txt extension.
